0acd043a6ca8fc0c066a046fbfa8fd5f56257972
Nilo-Jacome <nilitojacome@gmail.com>
2024-09-05 00:28:27 -0500

    commit 7
    actualizacion

--- createDatabase.js
+++ createDatabase.js
@@ -6,9 +6,10 @@ const client = new Client({
   port: process.env.DB_PORT,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   ssl: {
     require: true,
-    //rejectUnauthorized: false
+    rejectUnauthorized: false
   }
 });
 
